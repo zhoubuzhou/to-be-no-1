31a8e3ffa453465002172172ecf0a8d9de63d744
zhoubuzhou <101098173+zhoubuzhou@users.noreply.github.com>
2022-04-28 08:45:51 +0800

    mlk

---
+++ 2022-april/2022-4-27/a.js
@@ -0,0 +1,22 @@
+// let a = 10;
+// let b = 2;
+// console.log(a + b == 12);//ture
+// console.log(a >= b);
+// console.log(a <= b);
+// console.log(a + b != b + a);//false
+// a = 1;
+// console.log(a >= b);
+// let c = 3;
+// console.log(c == a + b);
+// console.log(a * b * 2 != c);
+
+let num = +prompt('请输入年龄');
+if(50>=num && num >= 18){
+    console.log('love');
+}
+
+// let nam = prompt('输入姓名');
+// if(!nam == false){
+//     console.log(nam);
+// }
+
---
+++ 2022-april/2022-4-27/b.js
@@ -0,0 +1,33 @@
+// const num = prompt('你有什么？');
+// if (num === '吃的') {
+//     console.log('我吃');
+// } else {
+//     console.log('我不');
+// }
+
+// const redd = prompt('今天周几');
+// if (redd === '周六') {
+//     console.log('休息');
+// } else if (redd === '周日') {
+//     console.log('休息');
+// } else if (redd === '不知道') {
+//     console.log('那就休息吧');
+// } else {
+//     console.log('上班');
+// }
+
+// let age = prompt('age?');
+// console.log(age >= 18 ? 'love' : 'no way');
+// let numer = age >= 50 ? 'old' : 'yang';
+// console.log(numer);
+
+// let day = prompt('今天周几');
+// let age = day === '周一' ? '上班' :
+//     day === '周二' ? '上班' :
+//         day === '周三' ? '上班' :
+//             day === '周四' ? '上班' :
+//                 day === '周五' ? '上班' :
+//                     day === '周六' ? '休息' :
+//                         day === '周日' ? '休息' :
+//                             '重新输入';
+// console.log(age);
